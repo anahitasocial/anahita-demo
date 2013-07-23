2fe984596309fc4469e0f38c304b0637dd94c981
Arash  Sanieyan <ash@peerglobe.com>
2013-07-22 22:53:03 -0700

    updated

--- tasks/example.task.php
+++ tasks/example.task.php
@@ -9,7 +9,8 @@
     ->setCode(function ( $input,  $output) use($console) {
     	$data = COMPOSER_ROOT.'/data/data.sql';
     	$console->runCommand("site:init --database-dump=$data --drop-database --database-prefix=demo");
-    	passthru("php anahita package:install connect photos topics pages todos");    	
+    	passthru("php anahita package:install connect photos topics pages todos html demo/demo");    	
+    	passthru("php anahita site:config --new-secret");    	    	
     })
     ;
 
